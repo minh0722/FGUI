b0c4c7a65c60043f7958b07f6d41ef04e6a8bb80
Minh Nguyen <minh0722@yahoo.com>
2023-11-01 00:05:38 +0200

    feat: more cleanup of imgui windows

--- source/main.cpp
+++ source/main.cpp
@@ -138,6 +138,8 @@ bool InitRendering()
     // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
     //                              NULL, io.Fonts->GetGlyphRangesJapanese());
     // IM_ASSERT(font != NULL);
+
+    return true;
 }
 
 void CleanupRendering()
@@ -151,6 +153,57 @@ void CleanupRendering()
     ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
 }
 
+bool show_demo_window = true;
+bool show_another_window = false;
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+void ShowHelloWorldWindow()
+{
+    static float f = 0.0f;
+    static int counter = 0;
+
+    ImGui::Begin("Hello, world!");              // Create a window called "Hello, world!" and append into it.
+
+    ImGui::Text("This is some useful text.");           // Display some text (you can use a format strings too)
+    ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+    ImGui::Checkbox("Another Window", &show_another_window);
+
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+    ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+    // Buttons return true when clicked (most widgets return true when edited/activated)
+    if (ImGui::Button("Button"))
+        counter++;
+    ImGui::SameLine();
+    ImGui::Text("counter = %d", counter);
+
+    if (ImGui::Button("Connect DB") && !g_ConnectionEstablished)
+    {
+        g_ConnectionEstablished = ConnectToDB();
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Test DB"))
+    {
+        // this blocks main thread whereas submit task does not
+        //TestDB();
+        BackgroundTaskScheduler::AddTask(TestDB);
+    }
+
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    ImGui::End();
+}
+
+void ShowAnotherWindow()
+{
+    // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when
+    // clicked)
+    ImGui::Begin("Another Window", &show_another_window);
+    ImGui::Text("Hello from another window!");
+    if (ImGui::Button("Close Me"))
+        show_another_window = false;
+    ImGui::End();
+}
+
 int main(int argc, char** args)
 {
     BackgroundTaskScheduler::Init();
@@ -159,10 +212,6 @@ int main(int argc, char** args)
     {
         return 1;
     }
-
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
     bool done = false;
@@ -193,50 +242,13 @@ int main(int argc, char** args)
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");              // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");           // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            // Buttons return true when clicked (most widgets return true when edited/activated)
-            if (ImGui::Button("Button"))
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            if (ImGui::Button("Connect DB") && !g_ConnectionEstablished)
-            {
-                g_ConnectionEstablished = ConnectToDB();
-            }
-            ImGui::SameLine();
-            if (ImGui::Button("Test DB"))
-            {
-                // this blocks main thread whereas submit task does not
-                //TestDB();
-                BackgroundTaskScheduler::AddTask(TestDB);
-            }
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
+            ShowHelloWorldWindow();
         }
 
         // 3. Show another simple window.
         if (show_another_window)
         {
-            // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when
-            // clicked)
-            ImGui::Begin("Another Window", &show_another_window);
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
+            ShowAnotherWindow();
         }
 
         // Rendering
